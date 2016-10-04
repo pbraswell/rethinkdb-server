1e6d6ffe327d3c477b31a84ee2df05faacd6fedc
Peter Braswell <peter.braswell@gmail.com>
2016-10-04 18:57:50 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# rethinkdb-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: rethinkdb-server
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
